86c97e1df8dd3286d10c55d10607f208939f9c78
eyemotion <eyemotiongamestudio@gmail.com>
2019-09-29 13:23:17 +0400

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Welcome First page')
+    res.render('Welcome')
 })
 
 module.exports = router
