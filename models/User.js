85a7b0889b75badd85933b1a0541c81ae94ccb78
eyemotion <eyemotiongamestudio@gmail.com>
2019-10-01 16:17:20 +0400

    updat

--- models/User.js
+++ models/User.js
@@ -1,29 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-
     name: {
         type: String,
         required: true
-    }
+    },
     email: {
         type: String,
         required: true
-    }
+    },
     name: {
         type: String,
         require: true
-    }
+    },
     password: {
         type: String,
         required: true
-    }
+    },
     date: {
         type: Date,
         default: Date.now
     }
-})
+});
 
-
-const User = mongoose.model('User', UserSchema)
-module.exports = User
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
